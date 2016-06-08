b2ec2ac372ab16733fa2f6205e04d78593848f8e
aaroncolclough <xaaroncx@live.co.uk>
2016-06-08 14:54:37 +0100

    changess

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var octopack = require('@octopusdeploy/gulp-octo');
+var del = require('del');
 
 // Simple package into bin directory
 gulp.task('pack', function() {
+        del(['./*.zip']);
 gulp.src(['./dist/**/*.*'], { base: './' })
         .pipe(octopack.pack('zip'))
         .pipe(gulp.dest('./'))
