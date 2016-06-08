a370c75cb398045020a50746ee6a6265a706ac71
aaroncolclough <xaaroncx@live.co.uk>
2016-06-08 14:38:44 +0100

    test

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,5 @@ var octopack = require('@octopusdeploy/gulp-octo');
 gulp.task('pack', function() {
 gulp.src(['./dist/**/*.*'], { base: './' })
         .pipe(octopack.pack('zip'))
-        .pipe(octopack.push({host: 'http://elysiantech.co.uk:4000/', apiKey: 'API-BRAYV52V5WVOAI2ABNZGOR3CZWE', replace: true}, 
-        function(err, result) {
-     if(!err) {
-        console.log("Package Pushed:" + body.Title + " v"+ body.Version +" (" + fileSizeString(body.PackageSizeBytes) +"nytes)"); 
-        }}))
+        .pipe(gulp.dest('./'))
 });
