16fa83b5641e5e550fa2b77ac50e23f8ddcf13f5
DeanMcgarrigle <dean.mcgarrigle@gmail.com>
2016-06-08 14:06:08 +0100

    updated production webpack file

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -26,10 +26,30 @@ module.exports = merge(baseWebpackConfig, {
       extract: true
     })
   },
+   resolve:{
+    alias: {
+        "TweenLite": Path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+        "TweenMax": Path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+        "TimelineLite": Path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+        "TimelineMax": Path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+        "ScrollMagic": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+        "animation.gsap": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+        "debug.addIndicators": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
+    }
+  },
   plugins: [
     // http://vuejs.github.io/vue-loader/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      "root.jQuery": "jquery",
+      Tether: "tether",
+      "window.Tether": "tether",
+      "scrollmagic": "scrollmagic"
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
